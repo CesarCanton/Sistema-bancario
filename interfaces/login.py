4da5c030166edb461bae06796f257d03417f41a1
Cesar <92348802+CesarCanton@users.noreply.github.com>
2026-02-21 22:35:05 -0600

    Validaciones y colores en consola agregados

--- interfaces/login.py
+++ interfaces/login.py
@@ -5,15 +5,25 @@ from interfaces.adminInterface import mostrarInterfazAdmin
 from interfaces.clienteInterface import mostrarInterfazCliente
 
 
-
+titulo = "\n\033[1;36;40m === LOGIN === \033[0m\n"
 def Login():
-    credenciales=input("Ingrese sus credenciales\n")
-    contra=input("Ingrese su contraseña\n")
-    rol=validarUsuario(credenciales,contra)
+    
+    while True:
+        os.system('cls')
+        print(titulo)
+        credenciales=input("Ingrese sus credenciales\n")
+        contra=input("Ingrese su contraseña\n")
+        rol=validarUsuario(credenciales,contra)
 
-    match rol:
-        case "ADMIN":
-            mostrarInterfazAdmin()
-            
-        case "CLIENTE":
-            mostrarInterfazCliente()
+        match rol:
+            case "ADMIN":
+                mostrarInterfazAdmin()
+                break
+            case "CLIENTE":
+                mostrarInterfazCliente()
+                break
+            case _:
+                print("\033[31mCredenciales incorrectas. Intente nuevamente.\033[0m")
+                input("Presione Enter para continuar...")
+                if __name__ == "__main__":    Login()
+        
