30117ae7476fad856f5f333a8bf1085e491e4583
Cesar <92348802+CesarCanton@users.noreply.github.com>
2026-02-23 18:08:10 -0600

    Metodo mostrar cuentas creado e implementado

--- services/adminService.py
+++ services/adminService.py
@@ -1,4 +1,5 @@
 from repositories.usuarioRepository import UsuarioRepository
+from repositories.cuentaRepository import CuentaRepository
 from models.usuario import Usuario
 
 def generar_username_admin(nombres, apellidos, id):
@@ -31,3 +32,12 @@ def listar_usuarios(usuarios):
               f"Apellidos: {usuario.apellidos}, DUI: {usuario.dui}, Rol: {usuario.rol}, Username: {usuario.username}")
         
 
+def listaDeCuentas():
+    cuentasRepo=CuentaRepository()
+    for cuenta in cuentasRepo.datos:
+        print(f"\033[1;36;40mId\033[0m {cuenta["id"]}. "
+              f"Tipo de cuenta: {cuenta["tipo"]}. "
+              f"Id del propietario: {cuenta["propietarioId"]}. "
+              f"Estado de cuenta: {cuenta["estado"]}. "
+              f"Saldo: {cuenta["saldo"]}\n")
+    
